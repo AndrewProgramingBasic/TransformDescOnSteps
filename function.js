6334e57f2b8db5854644351c9c19b07c293d456e
Andrade Leal Andrew Giovanovich <aandra05@cantv.com.ve>
2025-01-14 16:12:34 -0400

    Fixed Loader Css

--- function.js
+++ function.js
@@ -41,6 +41,9 @@ submitButton.addEventListener("click", async () => {
         loadingIndicator.style.display = "none";
     }
 });
+
+
+
 /*function seleccionarTexto() {
   const elemento = document.querySelector(".insertText");
   if (!elemento) {
